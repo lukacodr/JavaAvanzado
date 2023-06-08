f6680c4b054cd89e8e1d47b4b9f3ab8b1d2ba397
lukacodr <lucassabe2002@gmail.com>
2023-06-08 16:41:38 -0300

    progress

---
+++ src/FuncionLambda/FuncionMap/Main.java
@@ -0,0 +1,22 @@
+package FuncionLambda.FuncionMap;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+//La funcion .map aplica lo que yo le indico sobre cada elemento que recibo de un string
+public class Main {
+
+    public static void main(String[] args)  {
+        ArrayList<String> nombres = new ArrayList();
+        nombres.add("Paco");
+        nombres.add("Pepe");
+        nombres.add("Juan");
+
+        Stream<String> valores = nombres.stream().map(x -> x.toUpperCase());
+        valores.forEach(x -> System.out.println(x));
+        valores.forEach(x -> System.out.println(x));
+
+        //minuto 40
+
+    }
+}
