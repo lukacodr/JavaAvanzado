f720970b4fcfa78bc949126d48f0e845ef38dfa0
lukacodr <lucassabe2002@gmail.com>
2023-07-15 23:54:52 -0300

    progress

--- src/ServicioPOSTMAN/Main.java
+++ src/ServicioPOSTMAN/Main.java
@@ -1,7 +1,15 @@
 package ServicioPOSTMAN;
+// Postman es una aplicacion para realizar pruebas o peticiones contra API's utilizando el protocolo http
+// Y al obtener las respuestas se pueden ejecutar tests para verficar que algo funciona o que algo no funciona
+// Principalmente se utiliza a la hora de depurar API's o de invocar API's manualmente sin utilizar herramientas
+// de lineas de comando como curl
 
+//COMO CREAMOS UNA PETICION UTILIZANDO POSTMAN?
+// -Entremos a la coleccion que queramos, le damos a la flechita para que se despliegue
+// -Clikamos en add a request. Y se nos abrira el formulario de peticiones.
+// -En new request le vamos a dar un titulo a la peticion. En el desplegable de debajo utilizaremos uno de los metodos
+// -http que soporte mi destino osea mi aplicacion web
+// min 4
+//
 public class Main {
-
-    //esooo
-    //essooooooooooooowe
 }
